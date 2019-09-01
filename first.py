dac342cf7f0d118cd00c3e4334c748a78c007582
Digender <mdigender@gmail.com>
2019-09-01 09:36:33 +0530

    First Python program

---
+++ first.py
@@ -0,0 +1,32 @@
+#accept input
+
+print('Hello World')
+name = input('Who are you? Please enter your name!')
+print('Hello ' + name)
+
+
+print('Lets add two numbers')
+num1 = input('Please enter number 1: ');
+num2 = input('Please enter number 2: ');
+result = int(num1) + int(num2)
+print('Addition of '+str(num1)+ ' and ' + str(num2) + ' is : '+ str(result))
+
+print('Lets subtract two numbers')
+num1 = input('Please enter number 1: ');
+num2 = input('Please enter number 2: ');
+result = int(num1) - int(num2)
+print('Subtraction of '+str(num1)+ ' and ' + str(num2) + ' is : '+ str(result))
+
+
+print('Lets multiply two numbers')
+num1 = input('Please enter number 1: ');
+num2 = input('Please enter number 2: ');
+result = int(num1) * int(num2)
+print('Multiplication of '+str(num1)+ ' and ' + str(num2) + ' is : '+ str(result))
+
+
+print('Lets divide two numbers')
+num1 = input('Please enter number 1: ');
+num2 = input('Please enter number 2: ');
+result = int(num1) / int(num2)
+print('division of '+str(num1)+ ' and ' + str(num2) + ' is : '+ str(result))
